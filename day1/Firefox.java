56aa5209c9831ff8570b9b27112fc0391e7ba280
sateeshtest11 <127716252+sateeshtest11@users.noreply.github.com>
2026-01-03 20:37:26 +0530

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,74 @@
+package week1.day1;
+
+public class Firefox 
+{
+public static void main(String args[])
+{
+	//float browserVersion = 100.2f;
+	//String browserName ="firefox";
+	//boolean isVisible = true;
+	//System.out.println("browserVersion");
+	double browserVersion=100.2;
+	String browserName = "firefox";
+	boolean isVisible = true;
+	int releaseYear = 1998;
+	char browserLogo = 'f';
+	System.out.println("Browser Name    : " + browserName);
+	System.out.println("Browser Version    : " + browserVersion);
+	System.out.println("isVisible    : " + isVisible);
+	System.out.println("releaseYear    : " + releaseYear);
+	System.out.println("browserLogo    : " + browserLogo);
+	
+	int a = 10;
+	int b = 20;
+	int sum = a + b;
+	System.out.println("Total Sum is: " + sum);
+	
+	double price = 99.99;
+	double tax = 10.50;
+	double total = price + tax;
+	System.out.println("Total Price and Tax Calculation is : " + total);
+	
+	int marks = 75;
+	boolean isPassed = marks >= 35;
+	System.out.println("Total Pass Marksh should be greater than 35 : " + isPassed );
+	
+	int marks1 = 25;
+	boolean isFailed = marks1 <= 35;
+	System.out.println("Total Pass Marksh should be Less than 35 : " + isFailed );
+	
+	   // int example
+    int age = 30;
+    System.out.println("Age is: " + age);
+    
+ // double example
+    double salary = 55000.50;
+    System.out.println("Salary is: " + salary);
+    
+ // boolean example
+    boolean isEmployee = true;
+    System.out.println("Is Employee: " + isEmployee);
+    
+ // int (most commonly used whole numbers)
+    int salary1 = 50000;
+    System.out.println("int value (salary): " + salary1);
+
+    //long (very large whole numbers)
+    long population = 1400000000L;
+    System.out.println("long value (population): " + population);
+    
+   // float (decimal values – less precision)
+    float temperature = 36.5f;
+    System.out.println("float value (temperature): " + temperature);
+    
+    double price1 = 99.99;
+    System.out.println("double value (price): " + price1);
+    
+    char grade = 'A';
+    System.out.println("char value (grade): " + grade);
+
+}
+
+
+}
+
